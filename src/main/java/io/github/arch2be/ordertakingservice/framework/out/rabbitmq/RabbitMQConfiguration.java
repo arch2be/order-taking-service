f98dd087ccc34d34983d612dc73244cc037c7e76
Mateusz <jdvozef@gmail.com>
2024-02-28 23:58:38 +0100

    fix

--- src/main/java/io/github/arch2be/ordertakingservice/framework/out/rabbitmq/RabbitMQConfiguration.java
+++ src/main/java/io/github/arch2be/ordertakingservice/framework/out/rabbitmq/RabbitMQConfiguration.java
@@ -1,9 +1,14 @@
 package io.github.arch2be.ordertakingservice.framework.out.rabbitmq;
 
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +16,33 @@ import org.springframework.context.annotation.Configuration;
 class RabbitMQConfiguration {
     private final CachingConnectionFactory connectionFactory;
 
+    @Value("${publisher.queue-name:default-queue}")
+    private String queueName;
+
+    @Value("${publisher.exchange-name:default-exchange}")
+    private String exchangeName;
+
+    @Value("${publisher.routing-key:default-rk}")
+    private String routingKey;
+
     RabbitMQConfiguration(final CachingConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
     }
 
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
 
     @Bean
     Jackson2JsonMessageConverter converter() {
